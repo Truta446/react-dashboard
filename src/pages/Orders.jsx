c7d5545f11bcd4641badaf6fdaead64fc7409ab1
Juan Lopes <juan.versolato@outlook.com>
2022-07-17 10:55:13 -0300

    Chore: import code.

---
+++ src/pages/Orders.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  Inject,
+} from '@syncfusion/ej2-react-grids';
+
+import { Header } from '../components';
+import { ordersData, ordersGrid } from '../data/dummy';
+
+const Orders = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Orders;
